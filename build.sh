55077ff60e90585475adfd02e3aebd91a99fceeb
rodion.ugarov@sharp-dev.net <rodion.ugarov@sharp-dev.net>
2023-09-09 02:43:14 +0400

    Fix docker image name to redeploy inside portainer or another gitops tools

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # if arg 1 equals --no-cache
 if [ "$1" == "--no-cache" ]; then
-  docker build --no-cache -t my_terminal .
+  docker build --no-cache -t interview_coding_task_app .
 else
-  docker build -t my_terminal .
+  docker build -t interview_coding_task_app .
 fi
