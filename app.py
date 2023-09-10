e1781ea70f944024fac410d7d33eb6e6f98c5523
rodion.ugarov@sharp-dev.net <rodion.ugarov@sharp-dev.net>
2023-09-10 04:28:18 +0400

    colors + line breaks fixes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ load_dotenv()  # Load variables from .env file
 
 # Global variables
 real_token = os.environ.get("TOKEN")
-error_message = "\nError. Please provide token with --token=XXXXXXXXXXXXXXX parameter\n\n"
+error_message = "\nError. Please provide token with --token=XXXXXXXXXXXXXXX parameter\n"
 
 
 def main():
@@ -17,16 +17,16 @@ def main():
         elif sys.argv[1].startswith("--token") and len(sys.argv) > 2:
             token_to_challenge = sys.argv[2]
         else:
-            print(Fore.LIGHTRED_EX + error_message)
+            print(Fore.LIGHTRED_EX + error_message + "\n")
             exit()
     else:
-        print(Fore.LIGHTRED_EX + error_message)
+        print(Fore.LIGHTRED_EX + error_message + Style.RESET_ALL)
         exit()
 
     if real_token == token_to_challenge:
-        print(Fore.LIGHTGREEN_EX + "\nTask completed. Token is correct!\nCongratulations! :)\n\n")
+        print(Fore.LIGHTGREEN_EX + "\nTask completed. Token is correct!\n\nCongratulations! :)\n" + Style.RESET_ALL)
     else:
-        print(Fore.LIGHTRED_EX + "\nToken is incorrect! Task isn't completed!\n\n")
+        print(Fore.LIGHTRED_EX + "\nToken is incorrect! Task isn't completed!\n" + Style.RESET_ALL)
 
 
 if __name__ == '__main__':
