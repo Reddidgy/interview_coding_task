ff66f1aafb88afd9074bc5e01bcee61aa55e9982
rodion.ugarov@sharp-dev.net <rodion.ugarov@sharp-dev.net>
2023-09-10 04:06:10 +0400

    Colored messages in app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 from dotenv import load_dotenv
 import sys
+from colorama import Fore, Style
 
 load_dotenv()  # Load variables from .env file
 
 # Global variables
 real_token = os.environ.get("TOKEN")
-error_message = "Error. Please provide token with --token=XXXXXXXXXXXXXXX parameter"
+error_message = "\nError. Please provide token with --token=XXXXXXXXXXXXXXX parameter\n"
 
 
 def main():
@@ -16,16 +17,16 @@ def main():
         elif sys.argv[1].startswith("--token") and len(sys.argv) > 2:
             token_to_challenge = sys.argv[2]
         else:
-            print(error_message)
+            print(Fore.LIGHTRED_EX + error_message)
             exit()
     else:
-        print(error_message)
+        print(Fore.LIGHTRED_EX + error_message)
         exit()
 
     if real_token == token_to_challenge:
-        print("Task completed. Token is correct!\nCongratulations! :)")
+        print(Fore.LIGHTGREEN_EX + "\nTask completed. Token is correct!\nCongratulations! :)\n")
     else:
-        print("Token is incorrect! Task isn't completed!")
+        print(Fore.LIGHTRED_EX + "\nToken is incorrect! Task isn't completed!\n")
 
 
 if __name__ == '__main__':
